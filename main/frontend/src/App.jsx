e331948b1389f9f46752272dd5275c61ae53e352
AnanthpaV <135410310+AnanthpaV@users.noreply.github.com>
2024-10-16 12:47:48 +0530

    Update App.jsx

--- main/frontend/src/App.jsx
+++ main/frontend/src/App.jsx
@@ -12,6 +12,9 @@ import axios from 'axios';
 import PlacesPage from './components/pages/PlacesPage';
 import PlacesForm from './components/pages/PlacesForm';
 import PageForPlace from './components/pages/PageForPlace';
+import BookingsPage from './components/pages/BookingsPage';
+import BookingPage from './components/pages/BookingPage';
+import SearchResults from './components/SearchResults';
 
 axios.defaults.withCredentials = true;
 
@@ -28,7 +31,10 @@ function App() {
             <Route path="/account/places" element={<PlacesPage />} />
             <Route path="/account/places/new" element={<PlacesForm />} />
             <Route path="/account/places/:id" element={<PlacesForm />} />
-            <Route path="/place/:id" element={<PageForPlace/>}/>
+            <Route path="/place/:id" element={<PageForPlace />} />
+            <Route path="/account/bookings" element={<BookingsPage />} />
+            <Route path="/account/bookings/:id" element={<BookingPage />} />
+            <Route path="/search-results" element={<SearchResults />} /> {/* Moved SearchResults route inside the Layout */}
           </Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
